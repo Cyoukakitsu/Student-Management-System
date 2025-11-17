e6f04c7c469064e8030e3d84a32fdc63fe9aa947
ZHAO JIAYI <z261459808@gmail.com>
2025-11-17 22:38:37 +0900

    next

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,17 @@
-// import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Applayout from "./ui/Applayout";
+import ScoreList from "./features/score/ScoreList";
+
 function App() {
   return (
-    <>
-      <button className="btn"> Hello daisyUI</button>
-      <div className="collapse bg-base-100 border border-base-300">
-        <input type="radio" name="my-accordion-1" defaultChecked />
-        <div className="collapse-title font-semibold">
-          How do I create an account?
-        </div>
-        <div className="collapse-content text-sm">
-          Click the "Sign Up" button in the top right corner and follow the
-          registration process.
-        </div>
-      </div>
-      <div className="collapse bg-base-100 border border-base-300">
-        <input type="radio" name="my-accordion-1" />
-        <div className="collapse-title font-semibold ">
-          I forgot my password. What should I do?
-        </div>
-        <div className="collapse-content text-sm">
-          Click on "Forgot Password" on the login page and follow the
-          instructions sent to your email.
-        </div>
-      </div>
-      <div className="collapse bg-base-100 border border-base-300">
-        <input type="radio" name="my-accordion-1" />
-        <div className="collapse-title font-semibold">
-          How do I update my profile information?
-        </div>
-        <div className="collapse-content text-sm">
-          Go to "My Account" settings and select "Edit Profile" to make changes.
-        </div>
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Applayout />}>
+          <Route index element={<ScoreList />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
+
 export default App;
---
+++ src/features/score/ScoreList.jsx
@@ -0,0 +1,5 @@
+// import { useState } from 'react';
+function ScoreList() {
+  return <>ScoreList</>;
+}
+export default ScoreList;
