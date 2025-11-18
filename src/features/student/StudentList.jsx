6e42d91f262fe36e04700f9c622e180b4fc1aab3
ZHAO JIAYI <z261459808@gmail.com>
2025-11-18 22:58:35 +0900

    next

--- src/features/student/StudentList.jsx
+++ src/features/student/StudentList.jsx
@@ -1,5 +1,28 @@
 // import { useState } from 'react';
+import StudentListItem from "./StudentListItem";
+
 function StudentList() {
-  return <>StudentList</>;
+  return (
+    <div className="overflow-x-auto">
+      <table className="table">
+        {/* head */}
+        <thead>
+          <tr>
+            <th>
+              <label>
+                <input type="checkbox" className="checkbox" />
+              </label>
+            </th>
+            <th>Name</th>
+            <th>Class</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <StudentListItem />
+        </tbody>
+      </table>
+    </div>
+  );
 }
 export default StudentList;
